ddd85a5033a334f34e54f14377311fcb531b512b
xanofar <73376530+xanofar@users.noreply.github.com>
2022-12-14 13:31:35 -0600

    Update script.js

--- script.js
+++ script.js
@@ -15,8 +15,29 @@ generateBtn.addEventListener("click", writePassword);
 
 
 function generatePassword() {
-  var length = 8,
-      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&'()*+-./<=>?@[]\^_{}|~",
+var charset = ``
+  var length = prompt("How long would you like the password? Choose between 8 to 128 characters.")
+  if(length <8 || length >128){
+    return alert("Error! Password must be between 8 and 128 characters!")
+  }
+  var hasLowerCase = confirm("Do you want lower case letters? OK for yes, Cancel for no.")
+  var hasUpperCase = confirm("Do you want upper case letters? OK for yes, Cancel for no.")
+  var hasSpecialCharacters = confirm("Do you want special characters? OK for yes, Cancel for no.")
+  var lowerCase = `abcdefghijklmnopqrstuvwxyz`
+  var upperCase = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`
+  var specialCharacters = `0123456789!#$%&"'()*+-./<=>?@[]\^_{}|~`
+  if(hasLowerCase){
+charset += lowerCase
+  }
+  else if(hasUpperCase){
+charset += upperCase
+  }
+  else if(hasSpecialCharacters){
+charset += specialCharacters
+  }
+  else{
+    alert("Error! You must choose at least one character type!")
+  }
       retVal = "";
   for (var i = 0, n = charset.length; i < length; ++i) {
       retVal += charset.charAt(Math.floor(Math.random() * n));
